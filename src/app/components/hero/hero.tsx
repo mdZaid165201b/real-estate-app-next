c04acbc50096462fa761e35b0532500af4c7f77f
mdZaid165201b <45224@gcslahore.edu.pk>
2023-10-01 01:15:52 +0500

    Hero Section is in progress

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import HeroBG from '../../../../public/images/HeaderBG.jpg';
 function Hero() {
     return (
-        <header className='w-full h-[200px] md:h-[500px] lg:h-[700px] border border-red-700'>
-            <div className='z-50 h-full relative'>
+        <header className='w-full h-[200px] md:h-[500px] lg:h-[700px] border border-red-700 relative'>
+            <div className='z-20 h-full relative top-0 w-full'>
                 <Image
                     src={HeroBG}
                     alt='Hero Background'
@@ -12,10 +12,33 @@ function Hero() {
                 />
                 {/* overlay */}
                 <div className='w-full h-full bg-black absolute top-0 left-0 opacity-40'></div>
-                <div className=' text-white  h-[80px] absolute top-0 translate-y-56 left-[34%]'>
+                <div className=' text-white  h-[80px] absolute top-[40%] left-[30%]  flex flex-col justify-center items-center'>
                     <div className='flex justify-center items-center flex-col border border-teal-700'>
                         <h1 className='text-[57px]'>Find Your Dream Home</h1>
-                        <h1 className='text-center text-[25px]'>From low as $10 per day with limited time offer discounts.</h1>
+                        <h1 className='text-center text-[25px]'>From as low as $10 per day with limited time offer discounts.</h1>
+                    </div>
+                    <div className='w-full border border-gray-200 bg-white/80 rounded my-5 text-gray-700 flex justify-between items-center gap-x-4 p-2'>
+                        <div className=''>
+                            <input type="text" placeholder='Enter Keyword'  className='outline-none border rounded-xl p-3 focus:border-teal-500' />
+                        </div>
+                        <div>
+                            <div className='bg-white'>
+                                <select className='p-3'>
+                                    <option value="">Hello</option>
+                                    <option value="">Hello</option>
+                                    <option value="">Hello</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            Location
+                        </div>
+                        <div>
+                            Price
+                        </div>
+                        <div>
+                            Search
+                        </div>
                     </div>
 
                 </div>
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
         setContextMenu(!isContextMenuOpen);
     }
     return (
-        <header className='w-full border-[1px] border-b-teal-500 px-20 py-1'>
+        <header className='w-full border-[1px] border-b-teal-500 px-20 py-1 z-50'>
             <div className='w-full flex justify-between items-center'>
                 <div className='flex justify-center items-center'>
                     <Image
@@ -34,7 +34,7 @@ function Navbar() {
                         </div>
                         {
                             isContextMenuOpen && (
-                                <div className={`absolute right-10 top-10 bg-white border border-teal-500 min-w-[150px] rounded-lg z-50 shadow-md shadow-teal-500/50 `}>
+                                <div className={`absolute right-10 top-10 bg-white border border-teal-500 min-w-[150px] rounded-lg z-50 shadow-md shadow-teal-500/50 z-50`}>
                                     <div className='flex items-center justify-center  gap-x-2 w-full hover:bg-gray-300/30 px-4 py-2 rounded-lg cursor-pointer'>
                                         <div className='w-[100px] flex justify-start items-center'>
                                             <ImEnter size={20} className='mr-2' />
